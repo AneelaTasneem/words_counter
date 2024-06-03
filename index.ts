1ee8f3acf3cf5d7b7ea69a1c671bb9d7656dd92c
AneelaTasneem <aneelatasneem90@gmail.com>
2024-06-03 09:44:10 -0700

    first commit

---
+++ index.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+
+// Import the 'inquirer' module
+import inquirer from "inquirer";
+
+// Prompt the user to enter a sentence
+let user_response = await inquirer.prompt([
+  {
+    name: "sentence", // Save the user's input under the key 'sentence'
+    type: "input",
+    message: "Enter your sentence to count words."
+  }
+]);
+
+// Trim the sentence to remove any leading or trailing spaces,
+// then split the sentence into an array of words using spaces as the delimiter,
+// and get the length of the array to count the number of words
+let words_count = user_response.sentence.trim().split(" ").length;
+
+// Print the number of words to the console
+console.log(words_count);
